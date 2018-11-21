f18589c8b018be392b8191d9970f1d0e785c4104
Ivan Rubinson <ivanru@ac.sce.local>
2018-11-21 18:22:03 +0200

    Add tests

--- Tests.py
+++ Tests.py
@@ -14,6 +14,10 @@ class Test(unittest.TestCase):
 		self.assertEqual(ba.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, -12, -4), -10)
 		self.assertEqual(ba.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, 0, 2), 1)
 		self.assertEqual(ba.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, 0, 10), 3)
+		self.assertEqual(ba.findRoots(lambda x : x**2 - 25, -10, 0), -5)
+		self.assertEqual(ba.findRoots(lambda x : x**2 - 25, 0, 10), 5)
+		self.assertEqual(ba.findRoots(lambda x : x**2 - 25, -10, 3), -5)
+		self.assertEqual(ba.findRoots(lambda x : x**2 - 25, 3, 10), 5)
 		
 	def test_newtonrepson(self):
 		self.assertEqual(nr.findRoots(lambda x : x**2, lambda x : 2*x), 0)
@@ -24,6 +28,7 @@ class Test(unittest.TestCase):
 		self.assertEqual(nr.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, lambda x : 4*x**3 + 3*11*x**2 -7*2*x -155), -10)
 		self.assertEqual(nr.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, lambda x : 4*x**3 + 3*11*x**2 -7*2*x -155), 1)
 		self.assertEqual(nr.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, lambda x : 4*x**3 + 3*11*x**2 -7*2*x -155), 3)
+		self.assertEqual(nr.findRoots(lambda x : x**2 - 25, -10, lambda x : 2*x), 5)
 	
 	def test_secant(self):
 		self.assertEqual(sc.findRoots(lambda x : x**2, -10, 10), 0)
@@ -34,6 +39,10 @@ class Test(unittest.TestCase):
 		self.assertEqual(sc.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, -12, -4), -10)
 		self.assertEqual(sc.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, 0, 2), 1)
 		self.assertEqual(sc.findRoots(lambda x : x**4 + 11*x**3 -7*x**2 -155*x + 150, 0, 10), 3)
+		self.assertEqual(sc.findRoots(lambda x : x**2 - 25, -10, 0), -5)
+		self.assertEqual(sc.findRoots(lambda x : x**2 - 25, 0, 10), 5)
+		self.assertEqual(sc.findRoots(lambda x : x**2 - 25, -10, 3), -5)
+		self.assertEqual(sc.findRoots(lambda x : x**2 - 25, 3, 10), 5)
 	
 if __name__ == '__main__':
     unittest.main()
